61328352488a5fa8f538bad203df07ce19019c9d
Drayano <houari.rezk.1999@gmail.com>
2022-07-08 12:14:08 +0100

    Added remaining contact infos and the begginings of a URL Launcher scheme

---
+++ lib/screens/entreprises/entreprise_contact_detail.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:aea_app/providers/entreprise_provider.dart';
+
+class ContactInfo extends StatefulWidget {
+  final String id;
+
+  const ContactInfo({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  State<ContactInfo> createState() => _ContactInfoState();
+}
+
+class _ContactInfoState extends State<ContactInfo> {
+  @override
+  Widget build(BuildContext context) {
+    final EntrepriseProvider entrepriseModel = Provider.of<EntrepriseProvider>(context);
+    final int entrepriseId = int.parse(widget.id) - 1;
+
+    final bool fixePhone = (entrepriseModel.entrepriseList[entrepriseId].fixePhone != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].fixePhone != "");
+    final bool faxNumber = (entrepriseModel.entrepriseList[entrepriseId].faxNumber != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].faxNumber != "");
+    final bool mobile = (entrepriseModel.entrepriseList[entrepriseId].mobile != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].mobile != "");
+    final bool email = (entrepriseModel.entrepriseList[entrepriseId].email != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].email != "");
+    final bool website = (entrepriseModel.entrepriseList[entrepriseId].website != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].website != "");
+    final bool facebook = (entrepriseModel.entrepriseList[entrepriseId].facebook != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].facebook != "");
+    final bool youtube = (entrepriseModel.entrepriseList[entrepriseId].youtube != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].youtube != "");
+    final bool maps = (entrepriseModel.entrepriseList[entrepriseId].maps != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].maps != "");
+
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: fixePhone ? const Text("Fixe :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: fixePhone
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].fixePhone!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: fixePhone
+                    ? const Icon(
+                        Icons.phone,
+                        color: Colors.green,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: faxNumber ? const Text("Fax :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: faxNumber
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].faxNumber!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: faxNumber
+                    ? const Icon(
+                        Icons.fax,
+                        color: Colors.green,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: mobile ? const Text("Mobile :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: mobile
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].mobile!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: mobile
+                    ? const Icon(
+                        Icons.phone_android,
+                        color: Colors.green,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: email ? const Text("Email :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: email
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].email!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: email
+                    ? const Icon(
+                        Icons.email,
+                        color: Colors.red,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: website ? const Text("Site Web :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: website
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].website!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: website
+                    ? const Icon(
+                        Icons.language,
+                        color: Colors.blue,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: facebook ? const Text("Facebook :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: facebook
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].facebook!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: facebook
+                    ? const Icon(
+                        Icons.facebook,
+                        color: Colors.blue,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: youtube ? const Text("Youtube :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: youtube
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].youtube!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: youtube
+                    ? const Icon(
+                        Icons.ondemand_video_rounded,
+                        color: Colors.red,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: maps ? const Text("Google Maps :") : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
+                child: maps
+                    ? Text(entrepriseModel.entrepriseList[entrepriseId].maps!)
+                    : const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 0,
+                child: maps
+                    ? const Icon(
+                        Icons.navigation_rounded,
+                        color: Colors.green,
+                        size: 25,
+                      )
+                    : const SizedBox.shrink(), // Empty widget
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/entreprises/single_entreprise_detail.dart
+++ lib/screens/entreprises/single_entreprise_detail.dart
@@ -5,6 +5,7 @@ import 'package:readmore/readmore.dart';
 
 import 'package:aea_app/providers/entreprise_provider.dart';
 import 'package:aea_app/screens/views/entreprise_picture_slider.dart';
+import 'package:aea_app/screens/entreprises/entreprise_contact_detail.dart';
 
 class SingleEntrepriseDetail extends StatefulWidget {
   final String id;
@@ -26,9 +27,13 @@ class _SingleEntrepriseDetailState extends State<SingleEntrepriseDetail> {
 
     final EntrepriseProvider entrepriseModel = Provider.of<EntrepriseProvider>(context);
     final int entrepriseId = int.parse(widget.id) - 1;
+
+    final bool businessNameSize =
+        entrepriseModel.entrepriseList[entrepriseId].businessName.length < 18;
+
+    final bool logo = (entrepriseModel.entrepriseList[entrepriseId].logo != null) &&
+        (entrepriseModel.entrepriseList[entrepriseId].logo != "");
     const bool isVIP = true; //entrepriseModel.entrepriseList[entrepriseId].customerType;
-    final bool logo = entrepriseModel.entrepriseList[entrepriseId].logo != null;
-    final bool businessNameSize = entrepriseModel.entrepriseList[entrepriseId].businessName.length < 18;
 
     final bool companyPresentation =
         entrepriseModel.entrepriseList[entrepriseId].companyPresentation != null;
@@ -53,17 +58,19 @@ class _SingleEntrepriseDetailState extends State<SingleEntrepriseDetail> {
                 children: <Widget>[
                   Row(
                     children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          margin: const EdgeInsets.only(right: 0, bottom: 10),
-                          child: Image(
-                            image: AssetImage(
-                              "assets/entreprises/${entrepriseModel.entrepriseList[entrepriseId].logo}",
-                            ),
-                          ),
-                        ),
-                      ),
+                      logo
+                          ? Expanded(
+                              flex: 1,
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 0, bottom: 10),
+                                child: Image(
+                                  image: AssetImage(
+                                    "assets/entreprises/${entrepriseModel.entrepriseList[entrepriseId].logo}",
+                                  ),
+                                ),
+                              ),
+                            )
+                          : const SizedBox.shrink(), // Empty widget
                       Expanded(
                         flex: 2,
                         child: Column(
@@ -95,7 +102,7 @@ class _SingleEntrepriseDetailState extends State<SingleEntrepriseDetail> {
                                 color: Colors.amber,
                                 size: 30,
                               )
-                            : SizedBox.shrink(), // Empty widget
+                            : const SizedBox.shrink(), // Empty widget
                       ),
                     ],
                   ),
@@ -122,6 +129,16 @@ class _SingleEntrepriseDetailState extends State<SingleEntrepriseDetail> {
                       height: 1.6,
                     ),
                   ),
+                  Text(
+                    "Contact :",
+                    // "${entrepriseModel.entrepriseList[entrepriseId].businessName},",
+                    style: TextStyle(
+                      fontSize: businessNameSize ? 20 : 14,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromRGBO(74, 74, 74, .9),
+                    ),
+                  ),
+                  ContactInfo(id: widget.id)
                 ],
               ),
             )
